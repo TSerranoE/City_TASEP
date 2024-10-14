e79dce8aaf2916bb84e2f778c4e7ea792f4437e3
TSerranoE <156811950+TSerranoE@users.noreply.github.com>
2024-10-13 23:12:49 -0300

    versión lista para presentar

--- PAGINA_TASEP/src/App.tsx
+++ PAGINA_TASEP/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, /* useCallback, */ useEffect } from "react";
 import Grid from "./components/Grid";
-import ControlPanel from "./components/ControlPanel";
-import DataDisplay from "./components/DataDisplay";
-import CarForm from "./components/CarForm";
+// import ControlPanel from "./components/ControlPanel";
+// import DataDisplay from "./components/DataDisplay";
+// import CarForm from "./components/CarForm";
+// import MoveCarForm from "./components/MoveCarForm";
 import CreateCar from "./components/CreateCar";
-import MoveCarForm from "./components/MoveCarForm";
 import {
   calculateIntersections,
   calculateRowAndCols,
@@ -15,8 +15,8 @@ import StartButton from "./components/StartButton";
 function App() {
   const size = 50;
   const [clickedLines, setClickedLines] = useState<Set<string>>(new Set());
-  const [showIntersections, setShowIntersections] = useState(false);
-  const [showRowAndCols, setShowRowAndCols] = useState(false);
+  // const [showIntersections, setShowIntersections] = useState(false);
+  // const [showRowAndCols, setShowRowAndCols] = useState(false);
   const [cars, setCars] = useState<
     Array<{
       row: number;
@@ -25,11 +25,11 @@ function App() {
       id: string;
     }>
   >([]);
-  const [nextCarId, setNextCarId] = useState(1);
+  //const [nextCarId, setNextCarId] = useState(1);
   const [isActive, setIsActive] = useState(false);
 
   const intersections = calculateIntersections(clickedLines);
-  const { rowAndCols, calles } = calculateRowAndCols(clickedLines);
+  const { /* rowAndCols, */ calles } = calculateRowAndCols(clickedLines);
 
   useEffect(() => {
     const sendDataToBackend = async () => {
@@ -73,54 +73,69 @@ function App() {
   };
 
   useEffect(() => {
-  const fetchState = async () => {
-    try {
-      const response = await fetch("http://localhost:5000/state");
-      const data = await response.json();
+    const fetchState = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/state");
+        const data = await response.json();
 
-      const particulas = data['particulas'];
-      const particulas_agregadas = data['particulas_agregadas'];
+        type Particula = {
+          col: number;
+          color: string;
+          id: string;
+          row: number;
+        };
 
-      // Convert particulas_agregadas object to an array and iterate
-      Object.values(particulas_agregadas).forEach((particula) => {
-        const { col, color, id, row } = particula;
-        console.log("Adding car:", particula);
-        handleAddCar(col, row, color, id);
-      });
+        type ParticulaActualizada = {
+          id: string;
+          new_col: number;
+          new_row: number;
+        };
 
-      // Convert particulas object to an array and iterate
-      Object.values(particulas).forEach((particula) => {
-        const { id, new_col, new_row} = particula;
-        console.log("Updating car:", particula);
-        handleMoveCar(id, new_col, new_row);
-      });
+        const particulas: Record<string, ParticulaActualizada> =
+          data["particulas"];
+        const particulas_agregadas: Record<string, Particula> =
+          data["particulas_agregadas"];
 
-      //console.log("Received state from backend:", data);
-    } catch (error) {
-      console.error("Error fetching simulation state:", error);
-    }
-  };
+        // Convert particulas_agregadas object to an array and iterate
+        Object.values(particulas_agregadas).forEach((particula) => {
+          const { col, color, id, row } = particula;
+          console.log("Adding car:", particula);
+          handleAddCar(col, row, color, id);
+        });
+
+        // Convert particulas object to an array and iterate
+        Object.values(particulas).forEach((particula) => {
+          const { id, new_col, new_row } = particula;
+          console.log("Updating car:", particula);
+          handleMoveCar(id, new_col, new_row);
+        });
+
+        //console.log("Received state from backend:", data);
+      } catch (error) {
+        console.error("Error fetching simulation state:", error);
+      }
+    };
 
     const intervalId = setInterval(fetchState, 1000); // Fetch state every 0.5 seconds
 
     return () => clearInterval(intervalId); // Clean up on unmount
   }, []);
 
-  const toggleIntersections = () => {
-    setShowIntersections(!showIntersections);
-    setShowRowAndCols(false);
-  };
+  // const toggleIntersections = () => {
+  //   setShowIntersections(!showIntersections);
+  //   setShowRowAndCols(false);
+  // };
 
-  const toggleRowAndCols = () => {
-    setShowRowAndCols(!showRowAndCols);
-    setShowIntersections(false);
-  };
+  // const toggleRowAndCols = () => {
+  //   setShowRowAndCols(!showRowAndCols);
+  //   setShowIntersections(false);
+  // };
 
-  const getNextCarId = useCallback(() => {
-    const id = `car-${nextCarId}`;
-    setNextCarId((prevId) => prevId + 1);
-    return id;
-  }, [nextCarId]);
+  // const getNextCarId = useCallback(() => {
+  //   const id = `car-${nextCarId}`;
+  //   setNextCarId((prevId) => prevId + 1);
+  //   return id;
+  // }, [nextCarId]);
 
   const toggleStart = () => setIsActive((prev) => !prev);
 
@@ -149,8 +164,8 @@ function App() {
         </div>
       </div>
 
-      <p className={styles.instructions}>Scroll or press 'r' to rotate</p>
-      <ControlPanel
+      <p className={styles.instructions}>Scroll or press 'r' to rotate.</p>
+      {/* <ControlPanel
         showIntersections={showIntersections}
         showRowAndCols={showRowAndCols}
         onToggleIntersections={toggleIntersections}
@@ -170,7 +185,7 @@ function App() {
           getNextCarId={getNextCarId}
         />
         <MoveCarForm onMoveCar={handleMoveCar} gridSize={size} cars={cars} />
-      </div>
+      </div> */}
     </div>
   );
 }
