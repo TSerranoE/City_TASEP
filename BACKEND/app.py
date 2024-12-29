b61302779a50d49ccbf48f8af5623dab5093a827
TSerranoE <156811950+TSerranoE@users.noreply.github.com>
2024-12-29 17:56:14 -0300

    logs gpt

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -13,50 +13,40 @@ from Calles import Calles
 
 calles = Calles([])
 
-
 particulas_agregadas = []
 ultimo_id = 0
-size= 40
+size = 40
 isStart = False
 mode = 'paralelo'
 simulation_running = True
 step = 5
 cantidad_inicial = 100
 simulation_paused = threading.Event()
-simulation_paused.set() 
+simulation_paused.set()
 velocidad = 0.5
-#data_lock = threading.Lock()
 
 def run_simulation(calles):
     global particulas_agregadas
+    print("Simulation thread started...")  # Log de depuración
     while simulation_running:
         simulation_paused.wait()  # Espera si está en pausa
         if not isStart:
             time.sleep(0.1)  # Pequeña pausa para no consumir CPU innecesariamente
             continue
-        time.sleep(1.3-velocidad)
-        #global particulas_agregadas
-        #particulas_agregadas = calles.agregar_particulas_inicio(id, p=0.6)
-        #if len(particulas_agregadas) != 0:
-            #id = particulas_agregadas[-1].id + 1
+        time.sleep(1.3 - velocidad)
         print("Running simulation step...")  # Log de depuración
         calles.update_bloqueos()
-        if mode == 'secuencial': 
+        if mode == 'secuencial':
             calles.update_secuencial(0.5)
         else:
             calles.update_paralelo(0.5)
-        #calles.delete_particulas_posicion(size)
-        #for particula in particulas_agregadas:
-        #   if particula.posicion > size:
-        #       particulas_agregadas.remove(particula)
-        
 
 simulation_thread = threading.Thread(target=run_simulation, args=(calles,))
 simulation_thread.start()
 
 @app.route('/update_data', methods=['POST'])
 def update_data():
-    global size, isStart, simulation_paused, mode, step, cantidad_inicial,  ultimo_id, particulas_agregadas, velocidad
+    global size, isStart, simulation_paused, mode, step, cantidad_inicial, ultimo_id, particulas_agregadas, velocidad
     data = request.json
     extreme_points = data['calles']
     size = data['size']
@@ -81,7 +71,7 @@ def update_data():
         for extreme_point in extreme_points:
             direccion, posicion = extreme_point.split(";")
             calle = Calle(direccion=int(direccion), intersecciones=[], posicion=int(posicion))
-            # Densidad 
+            # Densidad
             posicion = density_init
             for _ in range(cantidad_inicial):
 
@@ -98,7 +88,7 @@ def update_data():
                     ultimo_id += 1
 
                 posicion -= step
-            calle.iniciar_altura(-size*2, size*2)
+            calle.iniciar_altura(-size * 2, size * 2)
             calle.update_bloqueo()
             calles.add_calle(calle)
             calles.update_intersecciones()
@@ -110,7 +100,7 @@ def get_state():
     global isStart
     if not isStart:
         return jsonify({"status": "paused", "message": "Simulation is paused"})
-    
+
     diccionario_particulas_agregadas = {}
     for particula in particulas_agregadas:
         direccion = particula.calle.direccion
@@ -135,7 +125,7 @@ def get_state():
         direccion = particula.calle.direccion
         if direccion == 0:
             columna = particula.calle.posicion
-            fila = particula.posicion 
+            fila = particula.posicion
         else:
             fila = particula.calle.posicion
             columna = particula.posicion
@@ -146,29 +136,26 @@ def get_state():
                 'new_col': columna
             }
 
-    diccionario_funcion_altura = {str(x)+','+str(y): 0 for x in range(size+1) for y in range(size+1)}   
-        
+    diccionario_funcion_altura = {str(x) + ',' + str(y): 0 for x in range(size + 1) for y in range(size + 1)}
+
     for calle in calles.calles:
         calle.actualizar_altura()
         direccion = calle.direccion
         intersecciones = {interseccion.posicion: interseccion.calles for interseccion in calle.intersecciones}
         if direccion == 0:
             y = calle.posicion
-            for x in range(size+1):
+            for x in range(size + 1):
                 if (x, y) in intersecciones.keys():
-                    diccionario_funcion_altura[str(x)+','+str(y)] = max(intersecciones[(x, y)][1].altura[y], calle.altura[x])
+                    diccionario_funcion_altura[str(x) + ',' + str(y)] = max(intersecciones[(x, y)][1].altura[y], calle.altura[x])
                 else:
-                    diccionario_funcion_altura[str(x)+','+str(y)] = calle.altura[x]
+                    diccionario_funcion_altura[str(x) + ',' + str(y)] = calle.altura[x]
         else:
             x = calle.posicion
-            for y in range(size+1):
+            for y in range(size + 1):
                 if (x, y) in intersecciones.keys():
-                    diccionario_funcion_altura[str(x)+','+str(y)] = max(intersecciones[(x, y)][0].altura[x], calle.altura[y])
+                    diccionario_funcion_altura[str(x) + ',' + str(y)] = max(intersecciones[(x, y)][0].altura[x], calle.altura[y])
                 else:
-                    diccionario_funcion_altura[str(x)+','+str(y)] = calle.altura[y]
-        
-
-
+                    diccionario_funcion_altura[str(x) + ',' + str(y)] = calle.altura[y]
 
     return jsonify({
         "particulas": diccionario_particulas,
@@ -177,4 +164,5 @@ def get_state():
     })
 
 if __name__ == '__main__':
+    print("Starting Flask app...")  # Log de depuración
     app.run(debug=True, use_reloader=False)
